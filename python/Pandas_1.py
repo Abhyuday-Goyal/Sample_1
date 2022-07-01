87e0e8d835455090b44105d82cf0c7844656bf80
Abhyuday-Goyal <abhyuday@ainorthstartech.com>
2022-07-01 22:14:29 +0530

    Pandas

--- python/Pandas_1.py
+++ python/Pandas_1.py
@@ -1,6 +1,9 @@
 for i in range (0,6):
     for j in range(0,5):
         print(i, end = '')
-        
+ 
+ for a in range (6,1,-1):
+     for b in range (5,1,-1):
+         print(j,end' ')       
         
         
